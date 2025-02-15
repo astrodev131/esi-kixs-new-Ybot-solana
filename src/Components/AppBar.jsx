b4c3762c859896dc1175576fe440516e5274de95
James <jamespk0309@gmail.com>
2025-02-15 03:27:43 -0600

    footer

--- src/Components/AppBar.jsx
+++ src/Components/AppBar.jsx
@@ -6,50 +6,61 @@ function AppBar() {
     return pathname === path ? "text-[#8c5aff]" : "";
   };
   return (
-    <div className="z-40 app-bar shadow-[1px -1px 8px rgba(0,0,0,0.5)]">
-      <nav className="app-bar-nav flex justify-around gap-2 relative p-2">
+    <div className="z-40 app-bar">
+      <nav className="app-bar-nav flex justify-between  relative py-2 px-6">
         <Link
           to="/"
-          className={`p-2 hover:bg-[#1D1C17] rounded-2xl z-10 ${getLinkClass("/")}`}
+          className={`p-2 flex flex-col justify-center items-center rounded-xl ${getLinkClass(
+            "/"
+          )} transition-all`}
         >
           <img src="/images/appbar/home.png" alt="home logo" width={28} />
+          <div className="text-xs mt-1 ">Home</div>
         </Link>
+
         <Link
-          to="/earn"
-          className={`p-2 hover:bg-[#1D1C17] rounded-2xl z-10 ${getLinkClass(
-            "/earn"
-          )}`}
+          to="/rewards"
+          className={`p-2 flex flex-col justify-center items-center rounded-xl ${getLinkClass(
+            "/rewards"
+          )} transition-all`}
         >
-          <img src="/images/appbar/task.png" alt="task logo" width={28} />
+          <img src="/images/appbar/task.png" alt="rewards logo" width={28} />
+          <div className="text-xs mt-1">Rewards</div>
         </Link>
-        <Link to="/store">
+        <Link to="/store" className={`${getLinkClass("/store")}`}>
           <img
             src="/images/appbar/store.png"
             alt="store logo"
-            className={`rounded-full absolute w-[128px] !h-[128px] top-3 left-[50%] -translate-x-1/2 -translate-y-1/2 ${getLinkClass(
-              "/store"
-            )}`}
+            className={`rounded-full absolute w-[64px] !h-[64px]  left-[51%] -translate-x-1/2 -translate-y-1/2 `}
           />
+          <div className="mt-10">Store</div>
         </Link>
         <Link
-          to="/daily-bonus"
-          className={`p-2 hover:bg-[#1D1C17] rounded-2xl z-10 ${getLinkClass(
-            "/daily-bonus"
-          )}`}
+          to="/earn"
+          className={`p-2 flex flex-col justify-center items-center rounded-xl ${getLinkClass(
+            "/earn"
+          )} transition-all`}
         >
           <img
             src="/images/appbar/daily_bonus.png"
-            alt="daily bonus"
+            alt="earn logo"
             width={28}
           />
+          <div className="text-xs mt-1">Earn</div>
         </Link>
+
         <Link
-          to="/league"
-          className={`p-2 hover:bg-[#1D1C17] rounded-2xl z-10 ${getLinkClass(
-            "/league"
-          )}`}
+          to="/portfolio"
+          className={`p-2 flex flex-col justify-center items-center rounded-xl ${getLinkClass(
+            "/portfolio"
+          )} transition-all`}
         >
-          <img src="/images/appbar/league.png" alt="league logo" width={28} />
+          <img
+            src="/images/appbar/league.png"
+            alt="portfolio logo"
+            width={28}
+          />
+          <div className="text-xs mt-1">Portfolio</div>
         </Link>
       </nav>
     </div>
